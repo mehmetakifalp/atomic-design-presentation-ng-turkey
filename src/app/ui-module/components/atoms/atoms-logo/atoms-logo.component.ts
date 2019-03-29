8e0261535f1df67d830b9358580196fdcb615596
mehmetakifalp <mehmetakifalp@gmail.com>
2019-03-29 16:34:49 +0300

    atoms logo

---
+++ src/app/ui-module/components/atoms/atoms-logo/atoms-logo.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit, ViewEncapsulation, ChangeDetectionStrategy, Input } from '@angular/core';
+
+@Component({
+  selector: 'atoms-logo',
+  templateUrl:'atoms-logo.component.html',
+  styleUrls: ['atoms-logo.component.css'],
+  encapsulation:ViewEncapsulation.None,
+  changeDetection:ChangeDetectionStrategy.OnPush
+})
+export class AtomsLogoComponent{
+
+
+
+}
