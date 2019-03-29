4cd73248357f67d61f6fe13443473a2b0244c807
mehmetakifalp <mehmetakifalp@gmail.com>
2019-03-29 17:04:09 +0300

    menu molecules model

--- src/app/ui-module/components/molecules/molecules-nav-menu/molecules-nav-menu.component.ts
+++ src/app/ui-module/components/molecules/molecules-nav-menu/molecules-nav-menu.component.ts
@@ -1,5 +1,9 @@
-import { Component, OnInit, ChangeDetectionStrategy, ViewEncapsulation } from '@angular/core';
-
+import { Component, OnInit, ChangeDetectionStrategy, ViewEncapsulation, Input } from '@angular/core';
+export interface MenuItem{
+  url:string;
+  icon:string;
+  text:string;
+}
 @Component({
   selector: 'molecules-nav-menu',
   templateUrl:'molecules-nav-menu.component.html',
@@ -8,6 +12,14 @@ import { Component, OnInit, ChangeDetectionStrategy, ViewEncapsulation } from '@
   encapsulation:ViewEncapsulation.None
 })
 export class MoleculesNavMenuComponent {
+
+    @Input()
+    menuItems: MenuItem[] = [
+      { url: '/login', icon:'glyphicon-envelope', text:'Profile'},
+      { url: '/login', icon:'glyphicon-plane', text:'Dashboard'},
+      { url: '/login', icon:'glyphicon-heart', text:'About'},
+      { url: '/login', icon:'glyphicon-star', text:'Favourites'},
+  ]
 
 
 }
