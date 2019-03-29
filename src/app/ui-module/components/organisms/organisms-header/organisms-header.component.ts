77f00fe0f1b469038d4c4d19b52c7b9a54ac431c
mehmetakifalp <mehmetakifalp@gmail.com>
2019-03-29 14:26:17 +0300

    molecules and organisms preview

---
+++ src/app/ui-module/components/organisms/organisms-header/organisms-header.component.ts
@@ -0,0 +1,9 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'organisms-header',
+  templateUrl:'organisms-header.component.html'
+})
+export class OrganismsHeaderComponent {
+  
+}
