16cf869c365d44c0b34c4505a9d31ce93d1429aa
mehmetalp <mehmetakifalp@gmail.com>
2019-05-18 01:22:21 +0300

    test

--- src/app/ui-module/components/atoms/atoms-link-with-icon/atoms-link-with-icon.component.ts
+++ src/app/ui-module/components/atoms/atoms-link-with-icon/atoms-link-with-icon.component.ts
@@ -1,16 +1,19 @@
-import { Component, ViewEncapsulation, ChangeDetectionStrategy, Input } from '@angular/core';
-import { AtomsLinkComponent } from '../atoms-link/atoms-link.component';
+import {
+  Component,
+  ViewEncapsulation,
+  ChangeDetectionStrategy,
+  Input
+} from "@angular/core";
+import { AtomsLinkComponent } from "../atoms-link/atoms-link.component";
 
 @Component({
-  selector: 'atoms-link-with-icon',
-  templateUrl:'atoms-link-with-icon.component.html',
-  encapsulation:ViewEncapsulation.None,
-  changeDetection:ChangeDetectionStrategy.OnPush
+  selector: "atoms-link-with-icon",
+  templateUrl: "atoms-link-with-icon.component.html",
+  encapsulation: ViewEncapsulation.None,
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class AtomsLinkWithIconComponent extends AtomsLinkComponent {
+  @Input() iconClass: string = "glyphicon ";
 
-  @Input() iconClass: string = 'glyphicon';
-
-  @Input() iconName: string = 'glyphicon-envelope';
-  
+  @Input() iconName: string = "glyphicon-envelope";
 }
